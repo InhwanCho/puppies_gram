d63e28694109e78926013a00dcd1a3d129391842
InhwanCho <whdlsghks2@gmail.com>
2024-02-01 15:15:37 +0900

    added delete btn in write post

--- src/pages/post/write.tsx
+++ src/pages/post/write.tsx
@@ -66,6 +66,10 @@ export default function Write() {
       setPhotoPreview(URL.createObjectURL(file))
     }
   }, [photo])
+  const onImageDelete = () => {
+    setPhotoPreview('')
+    resetField('image')
+  }
 
   //동영상
 
@@ -92,6 +96,7 @@ export default function Write() {
     resetField("video");
   };
 
+
   return (
     <Layout pageTitle="Write" hasBackBtn>
       <form
@@ -99,7 +104,29 @@ export default function Write() {
         className="w-full px-5 pb-20 flex flex-col"
       >
         {/* 이미지 첨부 영역 */}
-        {photoPreview ? <div className="youtubeContainer rounded-md border"><Image alt="image" fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" src={photoPreview} className="youtubeContaineriframe object-contain" /></div> : <label className="w-full mb-8 cursor-pointer text-gray-600 hover:border-amber-500/80 hover:text-amber-500/80 flex items-center justify-center border-2 border-dashed border-gray-300 h-[17rem] rounded-md">
+        {photoPreview ? <div>
+          <button
+            type="button"
+            onClick={onImageDelete}
+            className="flex float-right gap-2 mb-2 items-center text-right whitespace-nowrap text-xs text-gray-400"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.3"
+              stroke="#FD7B80"
+              className="w-5 h-5 text-right"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+              />
+            </svg>
+            <p>해당 이미지 삭제하기</p>
+          </button>
+          <div className="youtubeContainer rounded-md border"><Image alt="image" fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" src={photoPreview} className="youtubeContaineriframe object-contain" /></div></div> : <label className="w-full mb-8 cursor-pointer text-gray-600 hover:border-amber-500/80 hover:text-amber-500/80 flex items-center justify-center border-2 border-dashed border-gray-300 h-[17rem] rounded-md">
           <svg
             className="h-14 w-14"
             stroke="currentColor"
@@ -125,9 +152,6 @@ export default function Write() {
         {videoPreview ? <VideoPlayer videoId={videoPreview} /> : null}
         {/* 첨부 폼 */}
         <Videoinput register={register} previewStart={onVideoPreviewStart} previewDelete={onVideoPreviewDelete} />
-
-
-
 
 
         {/* 글 작성 영역 */}

32ae9cb969d9cbdf5fa061813f36e32f885dfcf1
InhwanCho <whdlsghks2@gmail.com>
2024-02-05 10:35:02 +0900

    fixing some error

--- src/pages/api/users/create-account.ts
+++ src/pages/api/users/create-account.ts
@@ -3,31 +3,32 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import client from "@/libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    const {
-      body: { email, password, name },
-    } = req;
-    console.log(email, password, name);
-    let user;
-    try {
-      user = await client.user.findUnique({
-        where: { email: email },
+  const {
+    body: { email, password, name },
+  } = req;
+  if (!email || !password) {
+    return res.status(404).end();
+  }
+  console.log(email, password, name);
+  let user;
+  try {
+    user = await client.user.findUnique({
+      where: { email: email },
+    });
+    if (user) {
+      return res.status(401).json({
+        ok: false,
+        errMsg: "이미 존재하는 이메일이 있습니다. 아이디 생성 실패",
       });
-      if (user) {
-        return res.status(401).json({
-          ok: false,
-          errMsg: "이미 존재하는 이메일이 있습니다. 아이디 생성 실패",
-        });
-      } else {
-        user = await client.user.create({
-          data: { email, password, name: name || "익명", avatar: "dog" },
-        });
-        return res.json({ ok: true });
-      }
-    } catch (error) {
-      console.log(error);
-      return res.status(500).end().json({ ok: false, error: error });
+    } else {
+      user = await client.user.create({
+        data: { email, password, name: name || "익명", avatar: "dog" },
+      });
+      return res.json({ ok: true });
     }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).end().json({ ok: false, error: error });
   }
 }
 
